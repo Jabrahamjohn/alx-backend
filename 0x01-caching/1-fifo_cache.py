5868cea18dee5e0eea17cddcb93a64b9df982be4
Jabrahamjohn <jabrahamjohns@gmail.com>
2024-04-02 19:42:32 +0300

    what's new i don't know

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -3,27 +3,28 @@
 
 from base_caching import BaseCaching
 
-def __init__(self):
-        """ Initiliaze
+class FIFOCache(BaseCaching):
+    """FIFO cache system that inherits from BaseCaching
+    and is a caching system
+    """
+    def __init__(self):
+        """Initiliaze
         """
         super().__init__()
         self.queue = []
-        self.cache_data = {}
 
-def put(self, key, item):
-        """ Add an item in the cache
+    def put(self, key, item):
+        """ adds an item in cache
         """
         if key and item:
-            if key in self.cache_data:
-                self.queue.remove(key)
-            elif len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
                 discard = self.queue.pop(0)
                 del self.cache_data[discard]
                 print("DISCARD: {}".format(discard))
-            self.queue.append(key)
             self.cache_data[key] = item
+            self.queue.append(key)
 
-def get(self, key):
-        """ Get an item by key
+    def get(self, key):
+        """ gets an item by key
         """
-        return self.cache_data.get(key, None)
+        return self.cache_data.get(key, None)   
